c495fe801e56150171aa72212b1c88d94413e92a
Asala20 <108730648+Asala20@users.noreply.github.com>
2023-05-20 00:49:47 -0700

    LogoutController name debugging

--- app/Http/Controllers/Api/LogoutController.php
+++ app/Http/Controllers/Api/LogoutController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class LogoutCountroller extends Controller
+class LogoutController extends Controller
 {
     public function logout(Request $request)
     {
