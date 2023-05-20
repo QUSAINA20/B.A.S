a71d4c31db18922f2342715e3a5075fd2e2242a3
Asala20 <108730648+Asala20@users.noreply.github.com>
2023-05-20 01:35:01 -0700

    SubscriberController error editing

--- app/Http/Controllers/Api/SubscriberController.php
+++ app/Http/Controllers/Api/SubscriberController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Models\Subscriber;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 
 class SubscriberController extends Controller
