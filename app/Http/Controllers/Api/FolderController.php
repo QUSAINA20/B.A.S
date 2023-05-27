d1da3791e3f577628c8e99b7a920dda040ec8a1e
QUSAINA20 <91684341+QUSAINA20@users.noreply.github.com>
2023-05-27 17:02:26 +0300

    send folder with the files inside

--- app/Http/Controllers/Api/FolderController.php
+++ app/Http/Controllers/Api/FolderController.php
@@ -113,7 +113,7 @@ class FolderController extends Controller
             $files = $folder->getMedia('documents');
 
             if ($files->isEmpty()) {
-                return response()->json(['message' => 'No files', 'files' => []]);
+                return response()->json(['message' => 'No files', 'files' => [], 'folder' => $folder]);
             }
 
             $fileData = $files->map(function ($file) {
@@ -123,7 +123,7 @@ class FolderController extends Controller
                 ];
             });
 
-            return response()->json(['files' => $fileData]);
+            return response()->json(['folder' => $folder, 'files' => $fileData]);
         }
     }
 
